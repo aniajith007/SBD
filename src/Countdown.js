97745cf34ed46bf3c693d36b74c8442a4d6301a3
aniajith007 <gajith234@gmail.com>
2024-03-22 17:54:03 +0530

    Future date bug fixed

--- src/Countdown.js
+++ src/Countdown.js
@@ -12,6 +12,7 @@ import Dropdown from './Test.js';
 import { saveAs } from 'file-saver';
 import banner1 from './Assets/SBanner1.png'
 import banner2 from './Assets/SBanner3.png'
+import { TypeAnimation } from 'react-type-animation';
 function CountdownTimer({ targetDate }) {
   const calculateTimeUntilBirthday = () => {
     const today = new Date();
@@ -38,12 +39,11 @@ function CountdownTimer({ targetDate }) {
   const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
 
-  const handleDownload = (image,name) => {
+  const handleDownload = (image, name) => {
     saveAs(image, name); // Save the image file when clicked
   };
 
   return (<>
-
     <div>
       <div className="bg-[#191A24] h-screen">
         <div className="flex flex-col items-center justify-center w-full h-full gap-8 sm:gap-16">
@@ -102,23 +102,42 @@ function CountdownTimer({ targetDate }) {
             </div>
           </div>}
           <div className="absolute left-0 top-50 mt-4 ml-4 flex flex-col gap-4">
-            <button className=" bg-white text-gray-400 p-2 rounded-md" onClick={() => handleDownload(banner1,"HBDSbanner1")}>
+            <button className=" bg-white text-gray-400 p-2 rounded-md" onClick={() => handleDownload(banner1, "HBDSbanner1")}>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 animate__animated animate__flash animate__infinite">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z" />
               </svg>
             </button>
-            <button className=" bg-white text-gray-400 p-2 rounded-md" onClick={() => handleDownload(banner2,"HBDSbanner2")}>
+            <button className=" bg-white text-gray-400 p-2 rounded-md" onClick={() => handleDownload(banner2, "HBDSbanner2")}>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 animate__animated animate__flash animate__infinite">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
               </svg>
-              
             </button>
           </div>
-          {<div className='flex absolute font-poppins animate__animated animate__flash animate__infinite text-xl text-white'>{format(targetDate, "dd-MM-yyyy")}</div>}
+
+          {/* <div className='flex relative font-poppins text-xl text-white'>
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed once, initially
+                'We produce food for Mice',
+                1000,
+                'We produce food for Hamsters',
+                1000,
+                'We produce food for Guinea Pigs',
+                1000,
+                'We produce food for Chinchillas',
+                1000,
+              ]}
+              speed={50}
+              //style={{ fontSize: '1rem', color: 'white' }}
+              repeat={Infinity}
+            />
+          </div> */}
+          {<div className='flex absolute font-poppins animate__animated animate__flash animate__infinite text-xl text-white'>
+            {format(targetDate, "dd-MM-yyyy")}</div>}
           {days == 365 ? <Lottie
             animationData={anim}
             loop={true}
-            style={{ width: 200, height: 200, display: "flex" }}
+            style={{ width: 180, height: 180, display: "flex" }}
           /> : <Lottie
             animationData={anim2}
             loop={true}
@@ -145,14 +164,30 @@ function Countdown() {
   const day = 24;
   var year = new Date().getFullYear();
   const month = 2;
-  if (new Date().getDate() > day && new Date().getMonth() == month - 1) {
+
+
+  if (new Date().getMonth() == month - 1 && new Date().getDate() > day) {
+    // For Same month and >24
     const date = new Date();
     const newDate = addYears(date, 1);
-    console.log("Next Your Spl Day : ", newDate)
+    console.log("from same month after bday Next Your Spl Day : ", newDate)
     year = newDate.getFullYear()
   }
+
+  if (new Date().getMonth() > month - 1) {
+    // For After feb month's
+    const date = new Date();
+    const newDate = addYears(date, 1);
+    console.log("After bdaymonth  bday Next Your Spl Day : ", newDate)
+    year = newDate.getFullYear()
+  }
+
+
+
+
   //console.log(day,year,month,new Date())
-  const targetDate = new Date(year, month - 1, day); // May 1, 2024
+  const targetDate = new Date(year, month - 1, day); // Feb 24, 2024
+  console.log("Target date : ", targetDate)
   return (
     <div>
       <CountdownTimer targetDate={targetDate} />
